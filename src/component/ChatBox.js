0150ec16cd113c561715a377bfdbaca786e9e0a6
RukeshB <rukeshbasukala@gmail.com>
2023-08-21 23:35:48 +0545

    added welcome page and chatroom page

---
+++ src/component/ChatBox.js
@@ -0,0 +1,9 @@
+const ChatBox = () => {
+    return (
+        <div className="">
+            <div className="text-black pt-[100px]">ChatRoom</div>
+        </div>
+    )
+}
+
+export default ChatBox;
---
+++ src/component/Welcome.js
@@ -0,0 +1,7 @@
+const Welcome = () => {
+    return (
+        <div className="text-black pt-[100px]">Welcome</div>
+    )
+}
+
+export default Welcome;
