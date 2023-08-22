fa1e9e7379f1d7507a3233ca34231184af106063
RukeshB <rukeshbasukala@gmail.com>
2023-08-22 12:30:21 +0545

    add feature to login and logout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import ChatBox from "./component/ChatBox";
 import Navbar from "./component/Navbar";
+import Welcome from "./component/Welcome";
+import { auth } from "./firebase";
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div>
       <Navbar/>
+      {user ? <ChatBox/> : <Welcome/>}
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,41 @@
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+
 const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const handleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
+  const handleSignOut = () => {
+    auth.signOut();
+  };
 
   return (
     <div className="fixed bg-[#0a192f] flex justify-between items-center w-screen px-10 py-2 h-14 text-white z-10">
       {/* logo */}
       <div className="text-xl">
-        Real <span className="font-bold font-2xl text-blue-700">Chat</span>
+        Real <span className="font-bold text-blue-700 font-2xl">Chat</span>
       </div>
 
-      <div className="text-xl hover:text-blue-700 cursor-pointer">
-        Login
-      </div>
-
-     
+      {user ? (
+        <div
+          className="text-xl cursor-pointer hover:text-red-700"
+          onClick={handleSignOut}
+        >
+          LogOut
+        </div>
+      ) : (
+        <div
+          className="text-xl cursor-pointer hover:text-blue-700"
+          onClick={handleSignIn}
+        >
+          Login
+        </div>
+      )}
     </div>
   );
 };
