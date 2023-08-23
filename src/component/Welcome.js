522d2c7e7afb0d08ea323f3caf17d4be3322086a
RukeshB <rukeshbasukala@gmail.com>
2023-08-23 23:00:42 +0545

    function to login

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -1,8 +1,12 @@
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import GoogleSignin from '../assets/image/btn_google_signin_dark_pressed_web.png'
+import { auth } from '../firebase';
 
 const Welcome = () => {
-  const googleSignIn = () => {
-  };
+    const handleSignIn = () => {
+      const provider = new GoogleAuthProvider();
+      signInWithRedirect(auth, provider);
+    };
 
   return (
     <main className="flex flex-col justify-center items-center text-[#7cc5d9] mt-10 p-[30px] w-full h-[80vh]">
@@ -11,7 +15,7 @@ const Welcome = () => {
       <p className="mb-5 text-gray-400">Sign in with Google to chat with with your fellow friends</p>
       <button className="bg-transparent border-[none]">
         <img
-          onClick={googleSignIn}
+          onClick={handleSignIn}
           src={GoogleSignin}
           alt="sign in with google"
           type="button"
