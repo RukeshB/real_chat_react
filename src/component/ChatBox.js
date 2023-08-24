6a307d569a2978c3d0be541855ac8107c387daed
RukeshB <rukeshbasukala@gmail.com>
2023-08-24 20:01:27 +0545

    fetch message from firebase database and render those message

--- src/component/ChatBox.js
+++ src/component/ChatBox.js
@@ -1,13 +1,53 @@
 import Message from "./Message";
 import SendMessage from "./SendMessage";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { useEffect, useState, useRef } from "react";
+import { db } from "../firebase";
 
 const ChatBox = () => {
+  const [message, setMessage] = useState([]);
+  const scroll = useRef();
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt", "desc"),
+      limit(50)
+    );
+
+    // onSnapshot function which listens to changes in the document
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const fetchedMessages = [];
+      querySnapshot.forEach((doc) => {
+        fetchedMessages.push({ ...doc.data(), id: doc.id });
+      });
+
+      const sortedMessages = fetchedMessages.sort(
+        (a, b) => a.createdAt - b.createdAt
+      );
+
+      setMessage(sortedMessages);
+    });
+
+    return () => unsubscribe;
+  }, []);
+
+  const messageComponents = message?.map((element) => {
+    return <Message key={element.id} message={element} />;
+  });
   return (
     <main className="chat-box">
       <div className="mb-[60px] p-[30px]">
-        <Message />
+        {messageComponents}
       </div>
-      <SendMessage />
+      <span ref={scroll}></span>
+      <SendMessage scroll={scroll} />
     </main>
   );
 };
