a0daec4813d95a6a5c7d5c05a60cd60852572fea
RukeshB <rukeshbasukala@gmail.com>
2023-08-24 20:01:43 +0545

    make data dynamic

--- src/component/Message.js
+++ src/component/Message.js
@@ -1,12 +1,14 @@
-const Message = () => {
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const Message = (props) => {
+    const [user] = useAuthState(auth);
   return (
-    <div className={`chat-bubble`}>
-      <img className="chat-bubble__left" src="" alt="user avatar" />
+    <div  className={`chat-bubble ${props.message.uid === user.uid ? "right" : ""}`}>
+      <img className="chat-bubble__left" src={props.message.avatar} alt="user avatar" />
       <div className="chat-bubble__right">
-        <p className="user-name">Timonwa Akintokun</p>
-        <p className="user-message">
-          We are building a real time chat app with React and Firebase.
-        </p>
+        <p className="user-name">{props.message.name}</p>
+        <p className="user-message">{props.message.text}</p>
       </div>
     </div>
   );
