22e21ab16193d13e4445385cb3e9974595b6796f
RukeshB <rukeshbasukala@gmail.com>
2023-08-24 19:00:54 +0545

    save message to firebase database

--- src/component/SendMessage.js
+++ src/component/SendMessage.js
@@ -1,8 +1,31 @@
 import { Button, Input } from "@material-tailwind/react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useState } from "react";
+import { auth, db } from "../firebase";
 
 const SendMessage = () => {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { uid, displayName, photoURL } = auth.currentUser;
+
+    await addDoc(collection(db, "messages"), {
+      text: message,
+      name: displayName,
+      avatar: photoURL,
+      createdAt: serverTimestamp(),
+      uid,
+    });
+
+    setMessage("");
+  };
+
   return (
-    <form className="fixed w-full bg-[#0a192f] px-[30px] py-5 bottom-0 rounded-md">
+    <form
+      onSubmit={handleSubmit}
+      className="fixed w-full bg-[#0a192f] px-[30px] py-5 bottom-0 rounded-md"
+    >
       <div className="relative flex w-full">
         <Input
           id="messageInput"
@@ -10,8 +33,11 @@ const SendMessage = () => {
           type="text"
           label="Enter Message"
           color="blue"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <Button
+            type="submit"
           size="sm"
           color="blue"
           className="!absolute right-1 top-1 rounded"
